5ba14aaa01a785988ae17750a9a4793060e71d0c
Karan Swami <karanswm934@gmail.com>
2025-09-06 17:24:15 +0530

    Create NewsContext

---
+++ src/Pages/Context/NewsContext.jsx
@@ -0,0 +1,51 @@
+import React, {createContext, useState, useEffect} from 'react'
+
+export const NewsContext = createContext();
+
+const newsProvider = ({ children }) => {
+    const [data, setData] = useState(null);
+    const [headlines, setHeadlines] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch('https://newsapi.org/v2/top-headlines/sources?apiKey=d983082ba3a1457cbd74e2e4886af9e2');
+                const data = await response.json();
+                setHeadlines(data.sources);
+            } catch (error) {
+                console.error('Error Fetching Headlines:',error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = "https://newsapi.org/v2/everything?q=everything&apiKey=d983082ba3a1457cbd74e2e4886af9e2";
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                const json = await response.json();
+                setData(json);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        fetchData();
+    }, []);
+    
+    return (
+        <NewsContext.Provider value={{ data, headlines, loading}}>
+            {children}
+        </NewsContext.Provider>
+    )
+}
+
+export default NewsContext
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <div className='home-card-container'>
+            <div className='card-conatiner'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
