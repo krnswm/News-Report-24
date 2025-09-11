8e410e33e1925944c84cfd3847bfddfe13e71190
Karan Swami <karanswm934@gmail.com>
2025-09-11 12:10:44 +0530

    Implement Infinite Scroll

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Home.css';
 import { NewsContext } from '../../Context/NewsContext';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Home = () => {
   const { data } = useContext(NewsContext);
+  const [displayedCount, setDisplayedCount] = useState(6);
 
   if (!data) return (
     <div className='loading-container'>
@@ -12,19 +14,32 @@ const Home = () => {
   );
 
   const articlesWithImages = data.articles.filter(article => article.urlToImage);
+  
+  const loadMoreData = () => {
+    setDisplayedCount(prevCount => Math.min(prevCount + 6, articlesWithImages.length))
+  }
 
   return (
     <div className='container'>
       <div className='masonry'>
-        {articlesWithImages.map((article, index) => (
-          <article key={index} className='masonry-item'>
-            <img src={article.urlToImage} alt={article.title} className='card-image' />
-            <div className='card-content'>
-              <h3 className='card-title'>{article.title}</h3>
-              <p className='card-description'>{article.description}</p>
-            </div>
-          </article>
-        ))}
+        <InfiniteScroll
+          dataLength = {displayedCount}
+          next = {loadMoreData}
+          hasMore = {displayedCount < articlesWithImages.length}
+          loader={<h4 style={{ textAlign: 'center', padding: '20px 0' }}>Loading...</h4>}
+        >
+          <div className = 'masonry-container'>
+            {articlesWithImages.slice(0, displayedCount).map((article, index) => (
+              <article key={index} className='masonry-item'>
+                <img src={article.urlToImage} alt={article.title} className='card-image' />
+                <div className='card-content'>
+                  <h3 className='card-title'>{article.title}</h3>
+                  <p className='card-description'>{article.description}</p>
+                </div>
+              </article>
+            ))}
+          </div>
+        </InfiniteScroll>
       </div>
     </div>
   );
